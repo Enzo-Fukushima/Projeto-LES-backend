d20057a012fc57d3d1d68ab4ff373e7e995d0bae
enzofukushima <enzo.f.uematsu@gmail.com>
2025-09-05 12:00:08 -0300

    feat: add Cliente field and mapToEntity method in EnderecoDTO for entity conversion

--- src/main/java/com/enzo/les/les/model/dtos/EnderecoDTO.java
+++ src/main/java/com/enzo/les/les/model/dtos/EnderecoDTO.java
@@ -2,6 +2,10 @@ package com.enzo.les.les.model.dtos;
 
 
 import com.enzo.les.les.enums.EstadoEnum;
+import com.enzo.les.les.model.entities.Cliente;
+import com.enzo.les.les.model.entities.Endereco;
+import com.enzo.les.les.enums.TipoLogradouroEnum;
+import com.enzo.les.les.enums.TipoResidenciaEnum;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -40,4 +44,24 @@ public class EnderecoDTO {
     
     @NotBlank(message = "Cliente ID não pode ser vazio")
     private Long clienteId;
+
+    private Cliente Cliente;
+
+    // 🔹 Conversão para entidade Endereco
+    public Endereco mapToEntity() {
+        Endereco endereco = new Endereco();
+        endereco.setId(this.id);
+        endereco.setTipoResidencia(TipoResidenciaEnum.valueOf(this.tipoResidencia));
+        endereco.setTipoLogradouro(TipoLogradouroEnum.valueOf(this.tipoLogradouro));
+        endereco.setLogradouro(this.logradouro);
+        endereco.setNumero(this.numero);
+        endereco.setBairro(this.bairro);
+        endereco.setCep(this.cep);
+        endereco.setCidade(this.cidade);
+        endereco.setEstado(this.estado);
+        endereco.setPais(this.pais);
+        endereco.setObservacoes(this.observacoes);
+        // Cliente deve ser setado no serviço
+        return endereco;
+    } 
 }
