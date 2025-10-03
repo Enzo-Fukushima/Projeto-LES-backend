19a443601ed6aec3930cd44876585675e9c9f259
Felipe Lopes <felpscokinho@gmail.com>
2025-10-03 13:46:03 -0300

    Feat: created new entity

---
+++ src/main/java/com/enzo/les/les/model/entities/SaldoEstoque.java
@@ -0,0 +1,24 @@
+package com.enzo.les.les.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "saldo_estoque")
+@Getter
+@Setter
+public class SaldoEstoque {
+
+    @Id
+    @Column(name = "livro_id")
+    private Long livroId;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "livro_id")
+    private Livro livro;
+
+    @Column(nullable = false)
+    private Integer quantidade = 0;
+}
