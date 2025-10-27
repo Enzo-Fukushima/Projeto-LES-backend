dbc76a99085b4ddba39139c2492166b775c696ef
Felipe Lopes <felpscokinho@gmail.com>
2025-10-27 04:55:26 -0300

    refactor: added attributes that were missing on cupomUseDTO

--- src/main/java/com/enzo/les/les/dtos/CupomUseDTO.java
+++ src/main/java/com/enzo/les/les/dtos/CupomUseDTO.java
@@ -1,30 +1,22 @@
 package com.enzo.les.les.dtos;
 
 import com.enzo.les.les.enums.TipoCupomEnum;
-import com.enzo.les.les.model.entities.Cupom;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class CupomUseDTO {
-    // Pode enviar id ou codigo (se quiser validar por código)
+    private Long id;
     private Long cupomId;
     private String codigo;
-    // valor aplicado (opcional - servidor pode calcular)
-    private TipoCupomEnum tipoCupom;
-
+    private TipoCupomEnum tipo;
     private Double valor;
-
-    public CupomUseDTO(Cupom cupom) {
-        CupomUseDTO dto = new CupomUseDTO();
-        dto.setCupomId(cupom.getId());
-        dto.setTipoCupom(cupom.getTipoCupom());
-        dto.setCodigo(cupom.getCodigo());
-    }
+    private Boolean percentual;
+    private Double valorMinimo;
+    private Boolean ativo;
+    private Boolean singleUse;
+    private String dataValidade;
 }
